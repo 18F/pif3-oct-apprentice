6df3d603dcab71c0378c311fff97341c52ce2895
Dan <dan.s.hammer@gmail.com>
2014-10-21 14:39:11 -0400

    reorient json to record format

--- py/clean.py
+++ py/clean.py
@@ -4,5 +4,14 @@ import pandas
 def convert_to_json(datadir='../data'):
     """Convert the raw CSV and save the JSON to disk"""
     data = pandas.io.parsers.read_csv(datadir + '/raw.csv')
-    data.to_json(datadir + '/raw.json')
+    data.to_json(datadir + '/raw.json', orient='records')
     return data
+
+
+import json
+from pprint import pprint
+json_data = open('../data/raw.json')
+
+data = json.load(json_data)
+pprint(data[0:2])
+json_data.close()
