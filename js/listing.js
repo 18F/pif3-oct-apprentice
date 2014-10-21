25b812d46f813e0ede3b4327a94d8ac6e61a4375
Aidan Feldman <aidan.feldman@gsa.gov>
2014-10-21 15:35:14 -0400

    list all fields

--- js/listing.js
+++ js/listing.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
   $.get('data/raw.json', function(data){
-    var markup = '';
-    data.forEach(function(el){
-      markup += '<tr><td>' + el.SPON_ORG_NAME + '</td></tr>';
-    });
+    var markup = data.reduce(function(lastRow, sponsor){
+      var fields = [
+        sponsor.SPON_ORG_NAME,
+        sponsor.NAICS_INDUSTRY,
+        sponsor.OCC_TITLE,
+        sponsor.SPON_ORG_CITY,
+        sponsor.SPON_ORG_STATE,
+        sponsor.PHONE_NUM,
+        sponsor.EMAIL
+      ];
+
+      var tds = fields.map(function(field){
+        return '<td>' + field + '</td>';
+      });
+
+      return lastRow + '<tr>' + tds.join() + '</tr>';
+    }, '');
 
     $('.sponsors').append(markup);
   });
