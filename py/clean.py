a29303a192c5707dda7c2fc1e1ea6fd676b77285
Dan <dan.s.hammer@gmail.com>
2014-10-21 14:34:18 -0400

    checking in json data of raw file and code to convert

--- py/clean.py
+++ py/clean.py
@@ -1,1 +1,8 @@
-import os
+import pandas
+
+
+def convert_to_json(datadir='../data'):
+    """Convert the raw CSV and save the JSON to disk"""
+    data = pandas.io.parsers.read_csv(datadir + '/raw.csv')
+    data.to_json(datadir + '/raw.json')
+    return data
